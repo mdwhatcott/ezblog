3a7b3ac7da2cda629f455274153f36088e393f47
Michael Whatcott <mdwhatcott@users.noreply.github.com>
2023-11-07 11:52:25 -0700

    100% test coverage! We're done now, right?

--- blog/post_test.go
+++ blog/post_test.go
@@ -1,5 +1,22 @@
 package blog
 
+import (
+	os2 "os"
+	"testing"
+
+	"github.com/mdwhatcott/ezblog/os"
+)
+
+const inputFile = `
+{
+	"slug":"/hello-world",
+	"title":"Greetings",
+	"author":"Michael Whatcott"
+}
++++
+### Hello, world!
+`
+
 const expectedContent = `<html>
 <head>
 	<title>Greetings</title>
@@ -14,3 +31,21 @@ const expectedContent = `<html>
 	</div>
 </body>
 </html>`
+
+func Test(t *testing.T) {
+	disk := map[string]string{"/input.md": inputFile}
+	os.ReadFile = func(path string) ([]byte, error) {
+		return []byte(disk[path]), nil
+	}
+	os.WriteFile = func(name string, data []byte, perm os2.FileMode) error {
+		disk[name] = string(data)
+		return nil
+	}
+
+	RenderPost("/input.md", "/output/")
+
+	actualContent := disk["/output/hello-world/index.html"]
+	if actualContent != expectedContent {
+		t.Errorf("\nwant: %s\ngot:  %s", expectedContent, actualContent)
+	}
+}
