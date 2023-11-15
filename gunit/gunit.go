39f4492280ec8759c0b4c1272d7ee9f877d16aa1
Michael Whatcott <mike@smarty.com>
2023-11-15 15:53:49 -0700

    gunit in ~10 lines!

--- gunit/gunit.go
+++ gunit/gunit.go
@@ -1,9 +1,24 @@
 package gunit
 
-import "testing"
+import (
+	"reflect"
+	"strings"
+	"testing"
+)
 
 func Run(f any, t *testing.T) {
-	// TODO
+	type_ := reflect.TypeOf(f)
+	for x := 0; x < type_.NumMethod(); x++ {
+		method := type_.Method(x).Name
+		if strings.HasPrefix(method, "Test") {
+			t.Run(method, func(t *testing.T) {
+				fixture := reflect.New(type_.Elem())
+				fixture.Elem().FieldByName("Fixture").Set(reflect.ValueOf(NewFixture(t)))
+				fixture.MethodByName("Setup").Call(nil)
+				fixture.MethodByName(method).Call(nil)
+			})
+		}
+	}
 }
 
 type Fixture struct{ *testing.T }
